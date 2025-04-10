57df2e48a42abed902d496bb7434dfe295053608
UstaBuva <betthadi@gmail.com>
2025-04-10 22:23:27 +0300

    HW1

--- src/main/java/BankSpringApplication.java
+++ src/main/java/BankSpringApplication.java
@@ -1,15 +1,9 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
-@EnableJpaRepositories
-@EnableTransactionManagement
-public class BankSpringApplication {
-
+@SpringBootApplication
+public class CurrencyApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BankSpringApplication.class, args);
+        SpringApplication.run(CurrencyApiApplication.class, args);
     }
 }
---
+++ src/main/java/model/CurrencyRepository.java
@@ -0,0 +1,5 @@
+import model.Currency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyRepository extends JpaRepository<Currency, String> {
+}
